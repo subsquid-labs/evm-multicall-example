be544db5e263aa4e8df0b286217015bc34442cf2
Alexander Belopashentsev <61732514+belopash@users.noreply.github.com>
2023-05-11 20:31:05 +0500

    Arrow squid (#14)
    * arrow-squid
    
    * use upsert instead of save

--- src/model/generated/transfer.model.ts
+++ src/model/generated/transfer.model.ts
@@ -1,34 +1,34 @@
 import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, ManyToOne as ManyToOne_, Index as Index_} from "typeorm"
 import {Token} from "./token.model"
-import {Owner} from "./owner.model"
+import {Account} from "./account.model"
 
 @Entity_()
 export class Transfer {
-  constructor(props?: Partial<Transfer>) {
-    Object.assign(this, props)
-  }
+    constructor(props?: Partial<Transfer>) {
+        Object.assign(this, props)
+    }
 
-  @PrimaryColumn_()
-  id!: string
+    @PrimaryColumn_()
+    id!: string
 
-  @Index_()
-  @ManyToOne_(() => Token, {nullable: true})
-  token!: Token
+    @Index_()
+    @ManyToOne_(() => Token, {nullable: true})
+    token!: Token
 
-  @Index_()
-  @ManyToOne_(() => Owner, {nullable: true})
-  from!: Owner
+    @Index_()
+    @ManyToOne_(() => Account, {nullable: true})
+    from!: Account
 
-  @Index_()
-  @ManyToOne_(() => Owner, {nullable: true})
-  to!: Owner
+    @Index_()
+    @ManyToOne_(() => Account, {nullable: true})
+    to!: Account
 
-  @Column_("timestamp with time zone", {nullable: false})
-  timestamp!: Date
+    @Column_("timestamp with time zone", {nullable: false})
+    timestamp!: Date
 
-  @Column_("int4", {nullable: false})
-  blockNumber!: number
+    @Column_("int4", {nullable: false})
+    blockNumber!: number
 
-  @Column_("text", {nullable: false})
-  txHash!: string
+    @Column_("text", {nullable: false})
+    txHash!: string
 }
--- src/processor.ts
+++ src/processor.ts
@@ -1,175 +1,43 @@
-import {BatchHandlerContext, BatchProcessorItem, EvmBatchProcessor, EvmBlock} from '@subsquid/evm-processor'
-import {LogItem} from '@subsquid/evm-processor/lib/interfaces/dataSelection'
-import {Store, TypeormDatabase} from '@subsquid/typeorm-store'
-import { lookupArchive } from '@subsquid/archive-registry'
-import {In} from 'typeorm'
-import {Owner, Token, Transfer} from './model'
-import {BigNumber} from 'ethers'
-import assert from 'assert'
-
+import {lookupArchive} from '@subsquid/archive-registry'
+import {
+    BlockHeader,
+    DataHandlerContext,
+    EvmBatchProcessor,
+    EvmBatchProcessorFields,
+    Log as _Log,
+    Transaction as _Transaction,
+} from '@subsquid/evm-processor'
+import {Store} from '@subsquid/typeorm-store'
 import * as erc721 from './abi/erc721'
-import {Multicall} from './abi/multicall'
 
 export const CONTRACT_ADDRESS = '0xac5c7493036de60e63eb81c5e9a440b42f47ebf5'
 export const MULTICALL_ADDRESS = '0x5ba1e12693dc8f9c48aad8770482f4739beed696'
 
-let database = new TypeormDatabase()
-let processor = new EvmBatchProcessor()
+export const processor = new EvmBatchProcessor()
     .setDataSource({
-        archive: lookupArchive('eth-mainnet'),
+        archive: 'https://v2.archive.subsquid.io/network/ethereum-mainnet',
         chain: 'https://rpc.ankr.com/eth',
     })
     .setBlockRange({
-        from: 15_584_000 ,
+        from: 15_584_000,
     })
-    .addLog(CONTRACT_ADDRESS, {
-        filter: [[erc721.events.Transfer.topic]],
-        data: {
-            evmLog: {
-                topics: true,
-                data: true,
-            },
-            transaction: {
-                hash: true,
-            },
+    .setFields({
+        evmLog: {
+            topics: true,
+            data: true,
+        },
+        transaction: {
+            hash: true,
         },
     })
+    .addLog({
+        address: [CONTRACT_ADDRESS],
+        topic0: [erc721.events.Transfer.topic],
+        transaction: true,
+    })
 
-type Item = BatchProcessorItem<typeof processor>
-type Context = BatchHandlerContext<Store, Item>
-
-processor.run(database, async (ctx) => {
-    let transfersData: TransferEventData[] = []
-
-    for (let block of ctx.blocks) {
-        for (let item of block.items) {
-            if (item.kind !== 'evmLog') continue
-
-            if (item.evmLog.topics[0] === erc721.events.Transfer.topic) {
-                transfersData.push(handleTransfer(ctx, block.header, item))
-            }
-        }
-    }
-
-    await saveTransfers(ctx, transfersData)
-})
-
-interface TransferEventData {
-    id: string
-    blockNumber: number
-    timestamp: Date
-    txHash: string
-    from: string
-    to: string
-    tokenIndex: bigint
-}
-
-function handleTransfer(
-    ctx: Context,
-    block: EvmBlock,
-    item: LogItem<{evmLog: {topics: true; data: true}; transaction: {hash: true}}>
-): TransferEventData {
-    let {from, to, tokenId} = erc721.events.Transfer.decode(item.evmLog)
-
-    let transfer: TransferEventData = {
-        id: item.evmLog.id,
-        tokenIndex: tokenId.toBigInt(),
-        from,
-        to,
-        timestamp: new Date(block.timestamp),
-        blockNumber: block.height,
-        txHash: item.transaction.hash,
-    }
-
-    return transfer
-}
-
-async function saveTransfers(ctx: Context, transfersData: TransferEventData[]) {
-    let tokensIds: Set<string> = new Set()
-    let ownersIds: Set<string> = new Set()
-
-    for (let transferData of transfersData) {
-        tokensIds.add(transferData.tokenIndex.toString())
-        ownersIds.add(transferData.from)
-        ownersIds.add(transferData.to)
-    }
-
-    let transfers: Transfer[] = []
-
-    let tokens = await ctx.store.findBy(Token, {id: In([...tokensIds])}).then((q) => new Map(q.map((i) => [i.id, i])))
-    let owners = await ctx.store.findBy(Owner, {id: In([...ownersIds])}).then((q) => new Map(q.map((i) => [i.id, i])))
-
-    let newTokens = await initTokens(
-        ctx,
-        last(ctx.blocks).header,
-        Array.from(tokensIds).filter((t) => !tokens.has(t))
-    )
-    newTokens.forEach((t) => tokens.set(t.id, t))
-
-    for (let transferData of transfersData) {
-        let from = owners.get(transferData.from)
-        if (from == null) {
-            from = new Owner({id: transferData.from})
-            owners.set(from.id, from)
-        }
-
-        let to = owners.get(transferData.to)
-        if (to == null) {
-            to = new Owner({id: transferData.to})
-            owners.set(to.id, to)
-        }
-
-        let tokenId = transferData.tokenIndex.toString()
-
-        let token = tokens.get(tokenId)
-        assert(token != null)
-        token.owner = to
-
-        let {id, blockNumber, txHash, timestamp} = transferData
-
-        let transfer = new Transfer({
-            id,
-            blockNumber,
-            timestamp,
-            txHash,
-            from,
-            to,
-            token,
-        })
-
-        transfers.push(transfer)
-    }
-
-    await ctx.store.save([...owners.values()])
-    await ctx.store.save([...tokens.values()])
-    await ctx.store.save(transfers)
-}
-
-async function initTokens(ctx: Context, block: EvmBlock, tokenIds: string[]) {
-    let contract = new Multicall(ctx, block, MULTICALL_ADDRESS)
-
-    let tokenURIs = await contract
-        .tryAggregate(
-            erc721.functions.tokenURI,
-            CONTRACT_ADDRESS,
-            tokenIds.map((id) => [BigNumber.from(id)]),
-            1000 // to prevent timeout we will use paggination
-        )
-        .then((rs) => rs.map((r) => (r.success ? r.value : 'unknown')))
-
-    let res: Token[] = new Array(tokenIds.length)
-    for (let i = 0; i < tokenIds.length; i++) {
-        res[i] = new Token({
-            id: tokenIds[i],
-            uri: tokenURIs[i],
-            index: BigInt(tokenIds[i]),
-        })
-    }
-
-    return res
-}
-
-function last<T>(arr: T[]): T {
-    assert(arr.length > 0)
-    return arr[arr.length - 1]
-}
+export type Fields = EvmBatchProcessorFields<typeof processor>
+export type Context = DataHandlerContext<Store, Fields>
+export type Block = BlockHeader<Fields>
+export type Log = _Log<Fields>
+export type Transaction = _Transaction<Fields>
