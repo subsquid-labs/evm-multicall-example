140ddba1a98033c596e21b70ca9eee14bf2db031
Alexander Belopashentsev <61732514+belopash@users.noreply.github.com>
2023-06-15 14:58:26 +0500

    ArrowSquid (#21)
    * Update dependency @subsquid/archive-registry to v2.1.10
    
    * Update dependency @subsquid/archive-registry to v2.1.11
    
    * Update dependency @subsquid/archive-registry to v2.1.12
    
    * Update dependency @subsquid/archive-registry to v2.1.13
    
    * Arrow squid (#14)
    
    * arrow-squid
    
    * use upsert instead of save
    
    * Update dependency @subsquid/evm-processor to v1.0.1
    
    * Update dependency @subsquid/archive-registry to v2.1.14
    
    * Update dependency @subsquid/evm-processor to v1.1.0
    
    * Update @subsquid
    
    * Update dependency @subsquid/evm-processor to v1.4.0
    
    * bump packages
    
    * use archive registry
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/model/generated/token.model.ts
+++ src/model/generated/token.model.ts
@@ -1,27 +1,27 @@
 import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, ManyToOne as ManyToOne_, Index as Index_, OneToMany as OneToMany_} from "typeorm"
 import * as marshal from "./marshal"
-import {Owner} from "./owner.model"
+import {Account} from "./account.model"
 import {Transfer} from "./transfer.model"
 
 @Entity_()
 export class Token {
-  constructor(props?: Partial<Token>) {
-    Object.assign(this, props)
-  }
+    constructor(props?: Partial<Token>) {
+        Object.assign(this, props)
+    }
 
-  @PrimaryColumn_()
-  id!: string
+    @PrimaryColumn_()
+    id!: string
 
-  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
-  index!: bigint
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    index!: bigint
 
-  @Index_()
-  @ManyToOne_(() => Owner, {nullable: true})
-  owner!: Owner
+    @Index_()
+    @ManyToOne_(() => Account, {nullable: true})
+    owner!: Account
 
-  @Column_("text", {nullable: false})
-  uri!: string
+    @Column_("text", {nullable: false})
+    uri!: string
 
-  @OneToMany_(() => Transfer, e => e.token)
-  transfers!: Transfer[]
+    @OneToMany_(() => Transfer, e => e.token)
+    transfers!: Transfer[]
 }
