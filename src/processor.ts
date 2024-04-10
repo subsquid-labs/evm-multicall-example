e8f01e1a74ee70546adf1e2c0764d7d769c697a4
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2024-04-10 15:53:50 +0200

    Modernization
    	new file:   .squidignore
    	modified:   commands.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -1,4 +1,3 @@
-import {lookupArchive} from '@subsquid/archive-registry'
 import {
     BlockHeader,
     DataHandlerContext,
@@ -14,11 +13,12 @@ export const CONTRACT_ADDRESS = '0xac5c7493036de60e63eb81c5e9a440b42f47ebf5'
 export const MULTICALL_ADDRESS = '0x5ba1e12693dc8f9c48aad8770482f4739beed696'
 
 export const processor = new EvmBatchProcessor()
-    .setDataSource({
-        archive: lookupArchive('eth-mainnet'),
-        chain: 'https://rpc.ankr.com/eth',
+    .setGateway('https://v2.archive.subsquid.io/network/ethereum-mainnet')
+    .setRpcEndpoint({
+        url: 'https://rpc.ankr.com/eth',
+        rateLimit: 10
     })
-    .setFinalityConfirmation(10)
+    .setFinalityConfirmation(75)
     .setBlockRange({
         from: 15_584_000,
     })
